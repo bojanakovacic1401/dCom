5648d24478f5e58be1a11e0fa5ba26214099b93a
bojanakovacic1401 <bojanakovacic1401@gmail.com>
2026-04-21 14:42:49 +0200

    Add project files.

---
+++ Modbus/ModbusFunctions/WriteSingleRegisterFunction.cs
@@ -0,0 +1,76 @@
+﻿using Common;
+using Modbus.FunctionParameters;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Modbus.ModbusFunctions
+{
+    /// <summary>
+    /// Class containing logic for parsing and packing modbus write single register functions/requests.
+    /// </summary>
+    public class WriteSingleRegisterFunction : ModbusFunction
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WriteSingleRegisterFunction"/> class.
+        /// </summary>
+        /// <param name="commandParameters">The modbus command parameters.</param>
+        public WriteSingleRegisterFunction(ModbusCommandParameters commandParameters) : base(commandParameters)
+        {
+            CheckArguments(MethodBase.GetCurrentMethod(), typeof(ModbusWriteCommandParameters));
+        }
+
+        /// <inheritdoc />
+        public override byte[] PackRequest()
+        {
+            ModbusWriteCommandParameters p = (ModbusWriteCommandParameters)CommandParameters;
+            byte[] request = new byte[12];
+
+            WriteUInt16(request, 0, p.TransactionId);
+            WriteUInt16(request, 2, p.ProtocolId);
+            WriteUInt16(request, 4, p.Length);
+            request[6] = p.UnitId;
+            request[7] = p.FunctionCode;
+            WriteUInt16(request, 8, p.OutputAddress);
+            WriteUInt16(request, 10, p.Value);
+
+            return request;
+        }
+
+        /// <inheritdoc />
+        public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
+        {
+            ValidateResponse(response);
+            Dictionary<Tuple<PointType, ushort>, ushort> retVal = new Dictionary<Tuple<PointType, ushort>, ushort>();
+            ushort address = ReadUInt16(response, 8);
+            ushort value = ReadUInt16(response, 10);
+            retVal.Add(new Tuple<PointType, ushort>(PointType.ANALOG_OUTPUT, address), value);
+            return retVal;
+        }
+
+        private void ValidateResponse(byte[] response)
+        {
+            if (response == null || response.Length < 12)
+            {
+                throw new ArgumentException("Invalid modbus response.");
+            }
+
+            byte responseFunctionCode = response[7];
+            if ((responseFunctionCode & 0x80) != 0)
+            {
+                HandeException(response[8]);
+            }
+        }
+
+        private ushort ReadUInt16(byte[] buffer, int startIndex)
+        {
+            return (ushort)((buffer[startIndex] << 8) | buffer[startIndex + 1]);
+        }
+
+        private void WriteUInt16(byte[] buffer, int startIndex, ushort value)
+        {
+            buffer[startIndex] = (byte)(value >> 8);
+            buffer[startIndex + 1] = (byte)(value & 0xFF);
+        }
+    }
+}
---
+++ ProcessingModule/AlarmProcessor.cs
@@ -0,0 +1,52 @@
+﻿using Common;
+
+namespace ProcessingModule
+{
+    /// <summary>
+    /// Class containing logic for alarm processing.
+    /// </summary>
+    public class AlarmProcessor
+    {
+        /// <summary>
+        /// Processes the alarm for analog point.
+        /// </summary>
+        /// <param name="eguValue">The EGU value of the point.</param>
+        /// <param name="configItem">The configuration item.</param>
+        /// <returns>The alarm indication.</returns>
+        public AlarmType GetAlarmForAnalogPoint(double eguValue, IConfigItem configItem)
+        {
+            if (eguValue < configItem.EGU_Min || eguValue > configItem.EGU_Max)
+            {
+                return AlarmType.REASONABILITY_FAILURE;
+            }
+
+            if (eguValue < configItem.LowLimit)
+            {
+                return AlarmType.LOW_ALARM;
+            }
+
+            if (eguValue > configItem.HighLimit)
+            {
+                return AlarmType.HIGH_ALARM;
+            }
+
+            return AlarmType.NO_ALARM;
+        }
+
+        /// <summary>
+        /// Processes the alarm for digital point.
+        /// </summary>
+        /// <param name="state">The digital point state</param>
+        /// <param name="configItem">The configuration item.</param>
+        /// <returns>The alarm indication.</returns>
+        public AlarmType GetAlarmForDigitalPoint(ushort state, IConfigItem configItem)
+        {
+            if (state > 1)
+            {
+                return AlarmType.ABNORMAL_VALUE;
+            }
+
+            return AlarmType.NO_ALARM;
+        }
+    }
+}
